a8195c02269fa194fecd7f716aac99bb15c08320
aliwert <154356044+aliwert@users.noreply.github.com>
2025-01-15 00:07:31 +0300

    feat: config middleware

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,10 @@ import (
 	"os"
 
 	"github.com/aliwert/fiber-example/database"
+	"github.com/aliwert/fiber-example/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
 )
 
@@ -19,13 +22,26 @@ func main() {
 	if err := database.Connect(); err != nil {
 		log.Fatal(err)
 	}
+
 	// create Fiber app
 	app := fiber.New()
+
+	// Add middleware
+	app.Use(logger.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+		AllowMethods: "GET, POST, PUT, DELETE, PATCH",
+	}))
+
+	// Setup routes
+	routes.SetupRoutes(app)
 
 	// start server
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
+	log.Printf("Server starting on port %s", port)
 	log.Fatal(app.Listen(":" + port))
 }
